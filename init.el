fcdc09bcaeafdaa7c0bee85238ac6aa5e4693570
P. Schreiber <schreiber.mmb@gmail.com>
2022-12-05 11:21:21 -0300

    load custom configs from /lisp/custom.el

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
 (require 'init-benchmarking) ;; measure startup time
 (require 'init-aesthetics)
 (require 'init-packages)
@@ -9,16 +12,3 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (unbind-key "C-z")
 (setq user-full-name "Pedro Schreiber")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(markdown-mode projectile vterm use-package solaire-mode rebecca-theme rainbow-mode rainbow-delimiters move-text ivy-rich doom-modeline counsel)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
