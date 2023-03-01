d33e8c0fa9a28e49c5040908ef17bc461bb82b08
P. Schreiber <schreiber.mmb@gmail.com>
2023-03-01 11:22:21 -0300

    push

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exec-path-from-shell reformatter elixir-mode erlang lsp-ivy lsp-mode which-key ivy-rich rainbow-delimiters solaire-mode rainbow-mode move-text counsel vterm use-package ivy doom-modeline)))
+   '(rust-mode exec-path-from-shell reformatter elixir-mode erlang lsp-ivy lsp-mode which-key ivy-rich rainbow-delimiters solaire-mode rainbow-mode move-text counsel vterm use-package ivy doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
