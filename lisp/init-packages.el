9acddf319b8b9e1cf1b5ca8aeb05a850dafd824e
Pedro Schreiber <schreiber.mmb@gmail.com>
2023-06-15 17:17:38 +0200

    add org mode export configs

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -28,7 +28,6 @@
   :config
   (setq
    dired-auto-revert-buffer t
-   dired-listing-switches "-laFGh1v --group-directories-first"
    dired-ls-F-marks-symlinks t 
    dired-recursive-copies 'always
    dired-dwim-target t))
@@ -56,29 +55,7 @@
 (use-package ivy-rich
   :init (ivy-rich-mode 1))
 
-(use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `corfu-excluded-modes'.
-  :init
-  (global-corfu-mode))
+(use-package corfu :init (global-corfu-mode))
 
 (use-package cape
   ;; Bind dedicated completion commands
@@ -148,9 +125,16 @@
 
 (use-package rust-mode)
 
+(use-package typescript-mode)
+
 (use-package elixir-mode)
 
 (use-package rust-mode)
+
+(use-package python-mode
+  :ensure t
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python3" . python-mode))
 
 (use-package lsp-mode
   :init
