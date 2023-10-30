bb41b9b2388ee0ffa2a7f6705a0aa8beb852c3f8
Pedro Schreiber <schreiber.mmb@gmail.com>
2023-10-30 14:15:40 -0300

    remove elixir support on org-mode (unused)

--- lisp/.custom.el
+++ lisp/.custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elisp-autofmt web-mode ox-spectacle ox-gfm reformatter lsp-ui go-projectile go-eldoc go-mode flycheck-haskell terraform-mode haskell-mode python-mode elixir-mode typescript-mode rust-mode erlang rainbow-delimiters rebecca-theme solaire-mode rainbow-mode move-text doom-modeline lsp-ivy projectile which-key counsel ivy-rich ivy flycheck docker magit vterm exec-path-from-shell)))
+    '(elisp-autofmt reformatter go-projectile go-eldoc go-mode terraform-mode haskell-mode python-mode elixir-mode typescript-mode rust-mode erlang web-mode ox-spectacle ox-gfm doom-modeline rebecca-theme rainbow-delimiters solaire-mode rainbow-mode move-text lsp-ivy projectile which-key counsel vertico ivy-rich ivy flycheck helpful restclient kubernetes docker magit multi-vterm vterm exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,8 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package
- org
+(use-package org
  :bind (("C-c a" . org-agenda))
  :custom
  (org-hide-leading-stars t)
@@ -14,15 +13,14 @@
  (org-babel-default-header-args:python '((:results . "output")))
  (org-latex-compiler "xelatex")
  (org-latex-pdf-process '("xelatex %f"))
- ;;(org-src-fontify-natively t)
- ;;(org-src-tab-acts-natively t)
+ ;; (org-src-fontify-natively t)
+ ;; (org-src-tab-acts-natively t)
  ;; (org-hide-emphasis-markers t)
  ;; (org-export-with-sub-superscripts nil)
  :config
  (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)
-    (elixir . t)
     (org . t)
     (shell . t)
     (js . t)
@@ -30,12 +28,10 @@
     (awk . t)
     (fortran . t)
     (python . t)
-    ;; (java . t)
-    ;;    (ditaa . t)
+    ;; (elixir . t)
     ;; (haskell . t)
     ;; (rust . t)
     ;; (http . t)
-    ;; (plantuml . t)
     )))
 
 (use-package ox-gfm)
