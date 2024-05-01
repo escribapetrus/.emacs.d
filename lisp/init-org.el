4b8c711a33cfc2e323d1b9ec4a60062b12900b31
Pedro Schreiber <schreiber.mmb@gmail.com>
2024-04-30 17:39:02 -0700

    add configuration for new document classes on org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -9,16 +9,28 @@
  (org-list-allow-alphabetical t)
  (org-pretty-entities t)
  (org-pretty-entities-include-sub-superscripts nil)
- (org-babel-python-command "python3")
  (org-babel-default-header-args:python '((:results . "output")))
  (org-latex-compiler "xelatex")
- (org-latex-pdf-process '("xelatex %f"))
- ;; (org-src-fontify-natively t)
+  (org-latex-pdf-process '("xelatex %f"))
+ ;; (org-babel-python-command "python3")
+  ;; (org-src-fontify-natively t)
  ;; (org-src-tab-acts-natively t)
  ;; (org-hide-emphasis-markers t)
  ;; (org-export-with-sub-superscripts nil)
+  ;;(setq org-latex-classes nil)
+
+  
  :config
- (org-babel-do-load-languages
+  (add-to-list 'org-latex-classes
+             '("simple-resume-cv"
+               "\\documentclass{simple-resume-cv}"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+
+  (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)
     (org . t)
@@ -30,15 +42,14 @@
     (python . t)
     (elixir . t)
     (restclient . t)
-    ;; (haskell . t)
-    ;; (rust . t)
-    ;; (http . t)
-    )))
+     )))
+
+
+
 
 (use-package ox-gfm)
 (use-package ox-spectacle)
 (use-package ob-elixir)
 (use-package ob-restclient)
-
 
 (provide 'init-org)
