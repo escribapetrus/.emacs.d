e54c067095a67d63e171c478a8b4af927030c301
Pedro Schreiber <schreiber.mmb@gmail.com>
2024-05-23 08:41:20 -0300

    add automatic lisp format on save

---
+++ lisp/.custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+    '(emacs-elisp-autofmt elisp-format which-key web-mode vertico typescript-mode terraform-mode solaire-mode rust-mode reformatter rebecca-theme rainbow-mode rainbow-delimiters python-mode ox-spectacle ox-gfm ob-restclient ob-elixir multi-vterm move-text magit lsp-ivy kubernetes ivy-rich helpful haskell-mode go-projectile flycheck exec-path-from-shell erlang elixir-mode elisp-autofmt doom-modeline docker counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/init-languages.el
+++ lisp/init-languages.el
@@ -2,15 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package flyspell
+(use-package
+ flyspell
  :ensure t
  :config
  (setq
   ispell-program-name "aspell"
   ispell-dictionary "en")
- (add-hook 'text-mode-hook #'flyspell-mode) (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+ (add-hook 'text-mode-hook #'flyspell-mode)
+ (add-hook 'prog-mode-hook #'flyspell-prog-mode))
 
-(use-package web-mode
+(use-package
+ web-mode
  :config
  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.eex?\\'" . web-mode)))
@@ -19,63 +22,71 @@
 
 (use-package erlang :defer t)
 
-(use-package rust-mode
-  :hook (rust-mode . eglot-ensure))
+(use-package rust-mode :hook (rust-mode . eglot-ensure))
 
 (use-package typescript-mode)
 
-(use-package elixir-mode
-  :config (projectile-register-project-type 'elixir '("mix.exs")
-                                  :project-file "mix.exs"
-				  :compile "mix compile"
-				  :test "mix test"
-				  :run "iex -S mix")
-  :hook
-  (before-save . elixir-format)
-  (elixir-mode . eglot-ensure))
+(use-package
+ elixir-mode
+ :config
+ (projectile-register-project-type
+  'elixir
+  '("mix.exs")
+  :project-file "mix.exs"
+  :compile "mix compile"
+  :test "mix test"
+  :run "iex -S mix")
+ :hook (before-save . elixir-format) (elixir-mode . eglot-ensure))
 
-(use-package python-mode
-  :ensure t
-  :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python3" . python-mode))
+(use-package
+ python-mode
+ :ensure t
+ :mode ("\\.py\\'" . python-mode)
+ :interpreter ("python3" . python-mode))
 
 (use-package haskell-mode :ensure t)
 
 (use-package terraform-mode :ensure t)
 
-;; (use-package go-mode
-;;   :ensure t
-;;   :config (projectile-register-project-type 'go '("go.mod")
-;;                                   :project-file "go.mod"
-;; 				  :compile "go build"
-;; 				  :test "go test"
-;; 				  :run "go run")
-;;   :hook
-;;   (before-save . gofmt))
+(use-package
+ go-mode
+ :ensure t
+ :config
+ (projectile-register-project-type
+  'go
+  '("go.mod")
+  :project-file "go.mod"
+  :compile "go build"
+  :test "go test"
+  :run "go run")
+ :hook (go-mode . gofmt-before-save) (go-mode . eglot-ensure))
 
-(use-package go-eldoc
-  :ensure t
-  :hook (go-mode . go-eldoc-setup))
+(use-package go-eldoc :ensure t :hook (go-mode . go-eldoc-setup))
 
-(use-package go-projectile
-  :ensure t
-  :bind (("C-c p p" . go-projectile-switch-project)
-          ("C-c p f" . go-projectile-test-package)
-          ("C-c p a" . go-projectile-toggle-between-implementation-and-test)))
+(use-package
+ go-projectile
+ :ensure t
+ :bind
+ (("C-c p p" . go-projectile-switch-project)
+  ("C-c p f" . go-projectile-test-package)
+  ("C-c p a" . go-projectile-toggle-between-implementation-and-test)))
 
-(use-package go-guru
-  :ensure t)
+(use-package go-guru :ensure t)
 
-(use-package go-rename
-  :ensure t
-  :bind (("C-c r" . go-rename)))
+(use-package go-rename :ensure t :bind (("C-c r" . go-rename)))
 
-(use-package eglot
-  :ensure nil
-  :config (add-to-list 'eglot-server-programs '(elixir-mode "/usr/local/bin/lexical/bin/start_lexical.sh")))
+(use-package
+ eglot
+ :ensure nil
+ :config
+ (add-to-list
+  'eglot-server-programs
+  '(elixir-mode "/usr/local/bin/lexical/bin/start_lexical.sh")))
 
-(use-package elisp-autofmt
- :commands (elisp-autofmt-mode elisp-autofmt-buffer)
- :hook (emacs-lisp-mode . elisp-autofmt-mode))
+(use-package
+ elisp-autofmt
+ :ensure t
+ :bind (("C-c f" . elisp-autofmt-buffer))
+ :hook (before-save . elisp-autofmt-buffer))
 
 (provide 'init-languages)
